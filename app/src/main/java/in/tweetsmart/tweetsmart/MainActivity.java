1695b54fc5e3b5e5929b35ff9cb627da9f00c5e6
Shashi Singh <singh.shashishekhar@gmail.com>
2015-09-16 23:41:19 +0530

    Got ToolBar to work on MainActivity. Added EditText to compose tweets and bound text change event to show char count.

--- app/src/main/java/in/tweetsmart/tweetsmart/MainActivity.java
+++ app/src/main/java/in/tweetsmart/tweetsmart/MainActivity.java
@@ -2,15 +2,47 @@ package in.tweetsmart.tweetsmart;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private Toolbar toolbar;
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        editText = (EditText) findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                TextView charCount = (TextView) findViewById(R.id.charCount);
+
+                charCount.setText(Integer.toString(s.length()));
+            }
+        });
     }
 
     @Override
